b1fa0dce230a8f6ba7d39e3f21ce220055e171bf
denismaxim0v <denis_maxim0v@protonmail.com>
2021-08-02 17:38:34 +0300

    wip: added metrics for feed contract metadata

--- main.go
+++ main.go
@@ -18,12 +18,15 @@ import (
 	"google.golang.org/grpc"
 )
 
-const FluxAggregator = "terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6"
+// const FluxAggregator = "terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6"
+// const FluxAggregator1 = "terra1z449mpul3pwkdd3892gv28ewv5l06w7895wewm"
+
+var aggregators = []string{"terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6", "terra1z449mpul3pwkdd3892gv28ewv5l06w7895wewm"}
 
 var (
-	ChainId       string
-	FeedId        string
-	ContractType  = "flux_monitor"
+	ChainId      string
+	ContractType = "flux_monitor"
+	// TODO what should the const labels be?
 	ConstLabels   map[string]string
 	rpcAddr       = os.Getenv("TERRA_RPC")
 	TendermintRpc = os.Getenv("TENDERMINT_RPC")
@@ -98,37 +101,53 @@ func getLatestBlockHeight() int64 {
 	return latestHeight
 }
 
-func setFeedId(grpcConn *grpc.ClientConn) {
-	sublogger := log.With().Str("request-id", uuid.New().String()).Logger()
-	wasmClient := wasmTypes.NewQueryClient(grpcConn)
+func getLatestRoundData(wasmClient wasmTypes.QueryClient, aggregatorAddress string) (*wasmTypes.QueryContractStoreResponse, error) {
 	response, err := wasmClient.ContractStore(
 		context.Background(),
 		&wasmTypes.QueryContractStoreRequest{
-			ContractAddress: FluxAggregator,
+			ContractAddress: aggregatorAddress,
+			QueryMsg:        []byte(`{"get_latest_round_data": {}}`),
+		},
+	)
+
+	return response, err
+}
+
+func getAggregatorConfig(wasmClient wasmTypes.QueryClient, aggregatorAddress string) (*wasmTypes.QueryContractStoreResponse, error) {
+	response, err := wasmClient.ContractStore(
+		context.Background(),
+		&wasmTypes.QueryContractStoreRequest{
+			ContractAddress: aggregatorAddress,
 			QueryMsg:        []byte(`{"get_aggregator_config": {}}`),
 		},
 	)
+
+	return response, err
+}
+
+func rawQuery(grpcConn *grpc.ClientConn) {
+	sublogger := log.With().Str("request-id", uuid.New().String()).Logger()
+	sublogger.Debug().Msg("querying RAW CONTRACT")
+	wasmClient := wasmTypes.NewQueryClient(grpcConn)
+	response, err := wasmClient.RawStore(context.Background(), &wasmTypes.QueryRawStoreRequest{
+		ContractAddress: aggregators[0],
+		Key:             []byte("aggregator_config"),
+	})
+
 	if err != nil {
 		sublogger.Error().Err(err).Msg("Could not query the contract")
-		return
 	}
 
-	var res AggregatorConfigResponse
-	err = json.Unmarshal(response.QueryResult, &res)
-
-	if err != nil {
-		sublogger.Error().Err(err).Msg("Could not parse response")
-		return
-	}
-
-	FeedId = res.Description
-	ConstLabels["feedId"] = FeedId
+	fmt.Println(response.Data)
+	sublogger.Debug().Msg(string(response.Data))
 }
 
 func TerraChainlinkHandler(w http.ResponseWriter, r *http.Request, grpcConn *grpc.ClientConn) {
 	sublogger := log.With().
 		Str("request-id", uuid.New().String()).
 		Logger()
+
+	// Gauges
 
 	nodeMetadataGauge := prometheus.NewGauge(
 		prometheus.GaugeOpts{
@@ -138,15 +157,16 @@ func TerraChainlinkHandler(w http.ResponseWriter, r *http.Request, grpcConn *grp
 		},
 	)
 
-	fmAnswersTotal := prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "flux_monitor_answers_total",
-			Help: "Reports the number of on-chain answers for a feed",
+	// TODO
+	feedContractMetadataGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "feed_contract_metadata",
+			Help: "Exposes metadata for individual feeds",
 		},
-		[]string{"contract_address"},
+		[]string{"chain_id", "contract_address", "contract_status", "contract_type", "feed_name", "feed_path", "network_id", "network_name", "symbol"},
 	)
 
-	fmLatestRoundResponseGauge := prometheus.NewGaugeVec(
+	fmAnswersGauge := prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "flux_monitor_answers",
 			Help: "Reports the current on chain price for a feed.",
@@ -162,13 +182,67 @@ func TerraChainlinkHandler(w http.ResponseWriter, r *http.Request, grpcConn *grp
 		[]string{"network_name", "chain_id", "network_id"},
 	)
 
+	// TODO
+	fmSubmissionReceivedValuesGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "flux_monitor_submission_received_values",
+			Help: "Reports the current submission value for an oracle on a feed",
+		},
+		[]string{"contract_address", "sender"},
+	)
+
+	// TODO: should be doable?
+	fmLatestRoundResponsesGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "flux_monitor_latest_round_responses",
+			Help: "Reports the current number of submissions received for the latest round for a feed",
+		},
+		[]string{"contract_address"},
+	)
+
+	// Counters
+	// Should be possible?
+	fmAnswersTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "flux_monitor_answers_total",
+			Help: "Reports the number of on-chain answers for a feed",
+		},
+		[]string{"contract_address"},
+	)
+
+	// TODO, maybe not possible
+	fmSubmissionsReceivedTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "flux_monitor_submissions_received_total",
+			Help: "Reports the current number of submissions",
+		},
+		[]string{"contract_address", "sender"},
+	)
+
+	// Should be okay
+	fmRoundsCounter := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "flux_monitor_rounds",
+			Help: "The number of rounds the monitor has observed on a feed",
+		},
+		[]string{},
+	)
+
+	// Register metrics
 	registry := prometheus.NewRegistry()
-	registry.MustRegister(fmLatestRoundResponseGauge)
+	registry.MustRegister(fmAnswersGauge)
+	registry.MustRegister(fmLatestRoundResponsesGauge)
 	registry.MustRegister(nodeMetadataGauge)
 	registry.MustRegister(fmAnswersTotal)
 	registry.MustRegister(fmCurrentHeadGauge)
+	registry.MustRegister(feedContractMetadataGauge)
+	registry.MustRegister(fmSubmissionReceivedValuesGauge)
+	registry.MustRegister(fmSubmissionsReceivedTotal)
+	registry.MustRegister(fmRoundsCounter)
 
 	var wg sync.WaitGroup
+
+	wasmClient := wasmTypes.NewQueryClient(grpcConn)
 
 	go func() {
 		defer wg.Done()
@@ -185,39 +259,68 @@ func TerraChainlinkHandler(w http.ResponseWriter, r *http.Request, grpcConn *grp
 
 	go func() {
 		defer wg.Done()
-		sublogger.Debug().Msg("Started querying flux monitor")
-		wasmClient := wasmTypes.NewQueryClient(grpcConn)
-		response, err := wasmClient.ContractStore(
-			context.Background(),
-			&wasmTypes.QueryContractStoreRequest{
-				ContractAddress: FluxAggregator,
-				QueryMsg:        []byte(`{"get_latest_round_data": {}}`),
-			},
-		)
-		if err != nil {
-			sublogger.Error().Err(err).Msg("Could not query the contract")
-			return
+		sublogger.Debug().Msg("Started querying aggregator answers")
+
+		for aggregator := range aggregators {
+			response, err := getLatestRoundData(wasmClient, aggregators[aggregator])
+
+			if err != nil {
+				sublogger.Error().Err(err).Msg("Could not query the contract")
+				return
+			}
+
+			var res LatestRoundResponse
+			err = json.Unmarshal(response.QueryResult, &res)
+
+			if err != nil {
+				sublogger.Error().Err(err).Msg("Could not parse the response")
+				return
+			}
+
+			fmAnswersGauge.With(prometheus.Labels{
+				"contract_address": aggregators[aggregator],
+			}).Set(float64(StringToInt(res.Answer)))
+
+			fmAnswersTotal.With(prometheus.Labels{
+				"contract_address": aggregators[aggregator],
+			}).Add(float64(res.RoundId))
 		}
+	}()
+	wg.Add(1)
 
-		var res LatestRoundResponse
-		err = json.Unmarshal(response.QueryResult, &res)
+	go func() {
+		defer wg.Done()
+		sublogger.Debug().Msg("Querying aggregators metadata")
 
-		if err != nil {
-			sublogger.Error().Err(err).Msg("Could not parse response")
-			return
+		for aggregator := range aggregators {
+			response, err := getAggregatorConfig(wasmClient, aggregators[aggregator])
+
+			if err != nil {
+				sublogger.Error().Err(err).Msg("Could not query the contract")
+				return
+			}
+
+			var res AggregatorConfigResponse
+			err = json.Unmarshal(response.QueryResult, &res)
+
+			if err != nil {
+				sublogger.Error().Err(err).Msg("Could not parse the response")
+				return
+			}
+
+			feedContractMetadataGauge.With(prometheus.Labels{
+				"chain_id":         ChainId,
+				"contract_address": aggregators[aggregator],
+				// TODO, should not be static
+				"contract_status": "active",
+				"contract_type":   ContractType,
+				"feed_name":       res.Description,
+				"feed_path":       "",
+				"network_id":      "",
+				"network_name":    "",
+				"symbol":          "",
+			})
 		}
-
-		fmt.Println(res)
-
-		fmLatestRoundResponseGauge.With(prometheus.Labels{
-			"contract_address": FluxAggregator,
-		}).Set(float64(StringToInt(res.Answer)))
-
-		fmAnswersTotal.With(prometheus.Labels{
-			"contract_address": FluxAggregator,
-		}).Add(float64(res.RoundId))
-
-		nodeMetadataGauge.Set(1)
 	}()
 	wg.Add(1)
 	wg.Wait()
@@ -239,7 +342,6 @@ func main() {
 	defer grpcConn.Close()
 
 	setChainId()
-	setFeedId(grpcConn)
 
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		TerraChainlinkHandler(w, r, grpcConn)
